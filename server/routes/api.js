d4e5140cbf3d1ad9e707cee49b94b5db0057bef7
alamnr <alamnr@gmail.com>
2018-10-06 01:15:34 +0600

    added server app  route for REST and create angular app,  routing,  bootstraping etc

--- server/routes/api.js
+++ server/routes/api.js
@@ -32,4 +32,54 @@ router.post('/register',(req,res)=>{
     });
 })
 
+router.post('/login', (req, res)=>{
+    let userData = req.body;
+    User.findOne({email:userData.email},(err,user)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(!user){
+                res.status(401).send('Invalid Email');
+            } else{
+                if(user.password !== userData.password){
+                    res.status(401).send('Invalid password');
+                }else{
+                    res.status(200).send(user);
+                }
+            }
+        }
+    });
+});
+
+router.get('/events',(req,res)=>{
+    let events = [
+        {
+            "_id":"1",
+            "name":"Auto Expo",
+            "description": "lorem ipsum",
+            "date":"2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id":"2",
+            "name":"Auto Expo",
+            "description": "lorem ipsum",
+            "date":"2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id":"3",
+            "name":"Auto Expo",
+            "description": "lorem ipsum",
+            "date":"2012-04-23T18:25:43.511Z"
+        },
+        {
+            "_id":"4",
+            "name":"Auto Expo",
+            "description": "lorem ipsum",
+            "date":"2012-04-23T18:25:43.511Z"
+        }
+    ];
+    res.json(events);
+})
+
 module.exports = router;
