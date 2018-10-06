2ba214eb4495928cb2dfa84048b62b3fbb54eeda
alamnr <alamnr@gmail.com>
2018-10-06 20:32:57 +0600

    tune the phone number directive

--- ngApp/src/app/shared/directive/phone-number.directive.ts
+++ ngApp/src/app/shared/directive/phone-number.directive.ts
@@ -4,13 +4,20 @@ import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 })
 export class PhoneNumberDirective {
 
-    regexStr =  '^[0-9 ()-+\t]*$';
+    regexStr =  '^[0-9 ()-+]*$';
     @Input() isPhoneNo: boolean;
 
     constructor(private el: ElementRef) { }
 
     @HostListener('keypress', ['$event']) onKeyPress(event) {
-        return new RegExp(this.regexStr).test(event.key);
+        //console.log(event.key + " " +  new RegExp(this.regexStr).test(event.key));
+        if(event.keyCode !== 8 && event.keyCode !== 46 && event.keyCode !== 9 && event.keyCode !== 37 && event.keyCode !== 39 ){
+            return new RegExp(this.regexStr).test(event.key);
+        }
+        else{
+            return true;
+        }
+       
     }
 
     @HostListener('paste', ['$event']) blockPaste(event: KeyboardEvent) {
