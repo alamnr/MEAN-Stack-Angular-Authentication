dc62e73b0d909a6170954d60c6da2d1a780e7495
Nur-A-Alam Sumon <alamnr@gmail.com>
2018-10-15 12:39:54 +0600

    Update unauthorize.component.ts

--- ngApp/src/app/unauthorize/unauthorize.component.ts
+++ ngApp/src/app/unauthorize/unauthorize.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-unauthorize',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnauthorizeComponent implements OnInit {
 
-  constructor() { }
+  httpCode:string;
+
+  constructor(private route: ActivatedRoute) { 
+    this.httpCode  =  this.route.snapshot.queryParamMap.get('code');
+    
+  }
 
   ngOnInit() {
+   
   }
 
 }
+
+
+
+// https://colorlib.com/wp/free-error-page-templates/
+
+// https://stackoverflow.com/questions/44864303/send-data-through-routing-paths-in-angular
