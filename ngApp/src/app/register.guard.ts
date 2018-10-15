ea5b80eedbde846bc55414ed3664ac594430f59b
Nur-A-Alam Sumon <alamnr@gmail.com>
2018-10-15 11:54:52 +0600

    Create register.guard.ts

---
+++ ngApp/src/app/register.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterGuard implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router)
+  {
+
+  }
+  canActivate(): boolean {
+    console.log(this.router.url);
+    if(this.authService.loggedIn()){
+      this.router.navigate(['/unauthorize']);
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+}
